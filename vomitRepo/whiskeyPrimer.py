72da272283c901a5f0b910287e8a6bf5cec0d734
Will Russell <whiskeyromeo06@gmail.com>
2016-09-06 15:36:34 -0400

    whiskeyPrimer update

--- vomitRepo/whiskeyPrimer.py
+++ vomitRepo/whiskeyPrimer.py
@@ -20,19 +20,26 @@ The comments are nested in each question
 def elementParser(filepath):
 	tree = ElementTree.parse(filepath)
 	root = tree.getroot()
+
 	threadList = []
 	for Thread in root.findall('Thread'):
 		QuestionDict = {}
 		relQuestion = Thread.find('RelQuestion')
 		subject = relQuestion.find('RelQSubject').text
+		#Pull the values from the questions into the relevant fields
 		QuestionDict['threadId'] = relQuestion.attrib['RELQ_ID']
 		QuestionDict['subject'] = subject
 		QuestionDict['question'] = relQuestion.find('RelQBody').text
-		comments = {}
+		comments = []
+		# Pull the comments in
 		for relComment in Thread.findall('RelComment'):
-			comments['comment'] = relComment.find('RelCText').text
-			comments['comment_id'] = relComment.attrib['RELC_ID']
+			commentDict = {}
+			commentDict['comment'] = relComment.find('RelCText').text
+			commentDict['comment_id'] = relComment.attrib['RELC_ID']
+			comments.append(commentDict)
+		# set the comments key to be equal to the question's comments
 		QuestionDict['comments'] = comments
+		#put the comments into the Question object
 		threadList.append(QuestionDict)
 	return threadList
 
