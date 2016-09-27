e21f753208bbb0b6f7d166552ba51c2741ee9beb
FreakofKnuth <freakofknuth@outlook.com>
2016-09-27 17:41:02 -0400

    Just import the function cosineSimilarity from the file cosineSimilarity and feed it a list with the new question vector and a list of lists all having the collection of questions vectors

---
+++ vomitRepo/cosineSimilarityTester.py
@@ -0,0 +1,12 @@
+from vomitRepo.cosineSimilarity import cosineSimilarity
+
+testQ = [1,2,3]
+testC = [[1,2,3],[-1,-2,-3],[-1,-2,3]]
+cosineMatrix = cosineSimilarity(testQ, testC)
+print(cosineMatrix)
+
+
+
+
+
+
