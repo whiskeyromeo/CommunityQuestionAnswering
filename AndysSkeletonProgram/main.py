83b3aa56e27dd28adf0c5dfe452e12aab55f0efa
Andy Fabian <fabianaj@vcu.edu>
2016-10-02 16:01:09 -0400

    Reworked loader not to not require a file list, added help text, Made loader skip questions with no question content

--- AndysSkeletonProgram/main.py
+++ AndysSkeletonProgram/main.py
@@ -1,8 +1,9 @@
 import sys
 import webbrowser
+import traceback
 sys.path.append('../HTMLOutput/')
 from HTMLOutput import HTMLOutput
-from loader import loadXMLQuestions
+from loader import loadXMLQuestions, getfilenames
 from preprocessWords import *
 from preprocessBigram import *
 from preprocessStopwords import *
@@ -10,15 +11,7 @@ from preprocessPartOfSpeech import *
 from featuresDoc2Vec import *
 from setup import setup
 from pprint import pprint
-
-def getargvalue(name, required):
-    output = False
-    for arg in sys.argv:
-        if arg[2:len(name)+2] == name:
-            output = arg[3+len(name):]
-    if required and not output:
-        raise Exception("Required argument " + name + " not found in sys.argv")
-    return output
+from utilities import *
 
 # Question-to-Question Module
 #
@@ -32,61 +25,77 @@ def getargvalue(name, required):
 
 ##############################################################################################
 
+# If no command line was given, display some help and then exit
+
+if len(sys.argv) < 2:
+    print("Usage: %s [--defaults] [--setup] [--questionfiles=file1,file2,file3] [--nostopwords] [--doc2vec]" % sys.argv[0])
+    exit(1)
+
+if "--defaults" in sys.argv:
+    sys.argv.append("--doc2vec")
+
 # Start up output system
 
 output = HTMLOutput()
 output.adddata("Command Line", sys.argv)
 
-# One-time setup
+try:
 
-if "--setup" in sys.argv:
-    setup()
+    # One-time setup
 
-# Load data from files
+    if "--setup" in sys.argv:
+        setup()
 
-print("Loading")
-questionFile = getargvalue("questionfile", True)
-QAData = loadXMLQuestions(questionFile)
-output.adddata("Loader", QAData)
+    # Load data from files
 
-# Pre-process: takes in question and outputs additional attributes, such as question_words
+    print("Loading")
+    questionFiles = getfilenames()
+    output.adddata("Loader: Files", questionFiles)
+    QAData = loadXMLQuestions(questionFiles)
+    output.adddata("Loader", QAData)
 
-print("PreProcessing: Words")
-QAData = preprocessAddWords(QAData, output)
-output.adddata("Words", QAData[0:2])
+    # Pre-process: takes in question and outputs additional attributes, such as question_words
 
-if "--nostopwords" not in sys.argv:
-    print("PreProcessing: Stopwords")
-    QAData = preprocessStopwords(QAData, output)
-    output.adddata("Stopwords: stopwords", preprocessStopwordsList())
-    output.adddata("Stopwords", QAData[0:2])
+    print("PreProcessing: Words")
+    QAData = preprocessAddWords(QAData, output)
+    output.adddata("Words", QAData[0:2])
 
-print("PreProcessing: Bigrams")
-QAData = preprocessBigram(QAData, output)
-output.adddata("Bigram", QAData[0:2])
+    if "--nostopwords" not in sys.argv:
+        print("PreProcessing: Stopwords")
+        QAData = preprocessStopwords(QAData, output)
+        output.adddata("Stopwords: stopwords", preprocessStopwordsList())
+        output.adddata("Stopwords", QAData[0:2])
 
-print("PreProcessing: Parts of Speech")
-QAData = preprocessPartOfSpeech(QAData, output)
-output.adddata("PartOfSpeech", QAData[0:2])
+    print("PreProcessing: Bigrams")
+    QAData = preprocessBigram(QAData, output)
+    output.adddata("Bigram", QAData[0:2])
 
-# Feature generation: takes question_words or other question attributes, outputs question_features_*
+    print("PreProcessing: Parts of Speech")
+    QAData = preprocessPartOfSpeech(QAData, output)
+    output.adddata("PartOfSpeech", QAData[0:2])
 
-if "--doc2vec" in sys.argv:
-    print("Feature Generation: Doc2Vec")
-    QAData = featuresdoc2vec(QAData, output)
-    output.adddata("Doc2Vec", QAData[0:2])
+    # Feature generation: takes question_words or other question attributes, outputs question_features_*
 
-# TODO: Other feature generators
+    if "--doc2vec" in sys.argv:
+        print("Feature Generation: Doc2Vec")
+        QAData = featuresdoc2vec(QAData, output)
+        output.adddata("Doc2Vec", QAData[0:2])
 
-# TODO: Merge all features into final feature set
+    # TODO: Other feature generators (issue 8)
 
-# Run/train the comparison system (iterate over each pair of questions)
+    # TODO: Merge all features into final feature set (issue 5)
 
-# TODO:
+    # Run/train the comparison system (iterate over each pair of questions) (issue 14)
 
-# Final output (sort scores)
+    # TODO:
 
-# TODO: Output in correct format for scoring system
+    # Final output (sort scores)
+
+    # TODO: Output in correct format for scoring system
+
+except Exception as e:
+    print(traceback.format_exc(), file=sys.stderr, flush=True)
+    output.addstring("EXCEPTION", traceback.format_exc())
 
 print("Output: Rendering HTML")
 outputpath = output.render()
---
+++ AndysSkeletonProgram/utilities.py
@@ -0,0 +1,17 @@
+import sys
+
+def getargvalue(name, required):
+    output = False
+    for arg in sys.argv:
+        if arg[2:len(name)+2] == name:
+            output = arg[3+len(name):]
+    if required and not output:
+        raise Exception("Required argument " + name + " not found in sys.argv")
+    return output
+
+def argvalueexists(name):
+    output = False
+    for arg in sys.argv:
+        if arg[2:len(name)+2] == name:
+            output = True
+    return output
