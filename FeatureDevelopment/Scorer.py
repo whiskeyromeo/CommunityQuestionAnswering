d8de5a7500d41a4d44a4ce3b0eaff2c66ce371f6
Andy Fabian <andy@afabian.com>
2016-10-31 13:27:48 -0400

    Added function to generate feature matrix, and stub code for Josh's iterative ranking algorithm

--- FeatureDevelopment/Scorer.py
+++ FeatureDevelopment/Scorer.py
@@ -21,6 +21,7 @@ class Scorer:
             for r in questions[q]['related']:
                 questions[q]['related'][r]['givenRank'] = scores.index(questions[q]['related'][r]['givenRank'])
 
+
     @staticmethod
     def score(questions):
         Scorer.scoreByCosine(questions)
@@ -31,3 +32,23 @@ class Scorer:
         for q in questions:
             for r in questions[q]['related']:
                 questions[q]['related'][r]['similarity'] = spatial.distance.cosine(questions[q]['featureVector'], questions[q]['related'][r]['featureVector'])
+
+
+    @staticmethod
+    def scoreByJOSH(questions):
+        for q in questions:
+            features = Scorer.getFeatureMatrix(questions[q])
+            # code should go here to run Josh's scoring algorithm,
+            # and then store the results in each related questions ['rank']
+
+
+    @staticmethod
+    def getFeatureMatrix(question):
+        features = []
+        features.append(question['featureVector'])
+        for r in question['related']:
+            features.append(question['related'][r]['featureVector'])
+        return features
+
+
+
