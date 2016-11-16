308949c8ce729001379778eb2fe74727f1f6ea47
Will Russell <whiskeyromeo06@gmail.com>
2016-11-16 16:04:18 -0500

    crawler finished

--- vomitRepo/crawler/qatarLiving_spider.py
+++ vomitRepo/crawler/qatarLiving_spider.py
@@ -9,28 +9,46 @@ import json
 from pprint import pprint
 import re
 
+def getTopic(self, response):
+	topic = response.request.url.split('/')
+	topic = re.sub('\?page=[0-9]', '',topic[len(topic)-1])
+	return topic
+
+def createSeedDict(topics = []):
+	seed = {}
+	for topic in topics:
+		seedDict = {
+			'topic': topic,
+			'url': 'http://www.qatarliving.com/forum/' + topic,
+			'page': 0,
+			'max_page': 5
+		}
+		seed[topic] = seedDict
+	return seed
+
+def seedStartUrls(seedDict):
+	start_urls = []
+	for k,v in seedDict.items():
+		start_urls.append(v['url'])
+	return start_urls
+
 class qatarLiving_spider(scrapy.Spider):
 	name = "ql"
-	start_urls = [
-		'http://www.qatarliving.com/forum/advice-help'
-	]
-	page = 0
-	postDict = {}
-	# uncomment the two lines below in order to hit all of the pages in the section
-	# NOTE: This may take a long time and potentially be looked at by an isp as being an attack
-	# causing a temporary cessation of services. 
-	# Current max page number from site/advice-help is 899
-	max_page = 10
-	#pages = response.css("div.b-pagination a.b-pagination--el-page::attr(href)").extract()
-	#max_page = re.sub('[^0-9]','',pages[len(pages)-1])
+	topics = ['advice-help', 'qatar-living-lounge','welcome-qatar','socialising','visas-permits','motoring', 'qatari-culture']
+	seedDict = createSeedDict(topics)
+	start_urls = seedStartUrls(seedDict)
+	currentTopic = 'STARTER'
+
 	f = open('./questFile.json', 'w')
 
 	def parseQuestions(self, response):
 		question = response.css("div.b-post-detail--el-text p::text").extract()
 		question = " ".join(question)
 		userDict = {
+			"subject": response.css("div.b-post-header--el-title::text").extract_first(),
 			"username": response.css("div.b-post-detail--el-info a::text").extract_first(),
-			"question": question
+			"question": question,
+			"topic": self.currentTopic
 		}
 		comments= response.css("div.b-comments-list--el-comment")
 		if(len(comments) > 0):
@@ -49,21 +67,36 @@ class qatarLiving_spider(scrapy.Spider):
 		if(userDict["question"] != None):
 			json.dump(userDict, self.f)
 			self.f.write('\n')
+	
 
 	def parse(self, response):
 		posts = response.css("a.b-topic-post--el-title::attr(href)").extract() 
-		postList = []
 		for post in posts:
-			#postList.append(scrapy.Request(post, self.parseQuestions))
 			yield scrapy.Request(post, self.parseQuestions)
-		#self.postDict[str(self.page)] = postList
-		#TODO : Figure out how to write the questions so that the topic is either carried with
-		# each individual question or there is a key with the topic for each set of questions from
-		# from that field
 		
-		if(self.page <= self.max_page):
-			href = '/forum/advice-help?page=' + str(self.page)
-			self.page += 1
+
+		currentTopic = getTopic(self, response)
+		self.currentTopic = currentTopic
+		currentPage = self.seedDict[currentTopic]['page']
+		currentMaxPage = self.seedDict[currentTopic]['max_page']
+		if(currentMaxPage == 5):
+			pages = response.css("div.b-pagination a.b-pagination--el-page::attr(href)").extract()
+			max_page = re.sub('[^0-9]','',pages[len(pages)-1])
+			#BEWARE!! --> uncommenting the following code will run through ALL of the pages on the site
+			# for advice-help : max-page = 899
+			# for qatar-living-lounge : max-page = 2487
+			# !!!!!!!!!!!!!
+			#self.seedDict[currentTopic]['max_page'] = max_page
+		print('##########')
+		print('topic : ' + currentTopic)
+		print('url : ' + response.request.url)
+		print('page: ' + str(currentPage))
+		print('real max_page: ' + str(max_page))
+		print('##########')
+
+		if(currentPage <= currentMaxPage):
+			href = '/forum/' + currentTopic + '?page=' + str(currentPage)
+			self.seedDict[currentTopic]['page'] += 1
 			next_page = response.urljoin(href)
 			yield scrapy.Request(next_page, self.parse)
 		
