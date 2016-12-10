0ef52cd14071d05eb4c8692a379d3ac523560712
Will Russell <whiskeyromeo06@gmail.com>
2016-12-10 13:34:35 -0500

    fixed rmodelRunner.py

--- FinalProject/modelRunner.py
+++ FinalProject/modelRunner.py
@@ -18,9 +18,9 @@ print('###################################')
 os.chdir('..')
 for file in filenames:
 	if 'subtaskA' in file:
-		os.system("gnome-terminal -e 'bash -c \"python ./scorer/MAP_scripts/ev.py ./scorer/SemEval2016-Task3-CQA-QL-dev-subtaskA.xml.subtaskA.relevancy ./models/" + file +";exec bash\"'")
+		os.system("gnome-terminal -e 'bash -c \"python -c 'print(\'OUTPUT FROM "+ file +"\'); python ./scorer/MAP_scripts/ev.py ./scorer/SemEval2016-Task3-CQA-QL-dev-subtaskA.xml.subtaskA.relevancy ./models/" + file +";exec bash\"'")
 	else:
-		os.system("gnome-terminal -e 'bash -c \"python ./scorer/MAP_scripts/ev.py ./scorer/SemEval2016-Task3-CQA-QL-dev.xml.subtaskB.relevancy ./models/" + file +";exec bash\"'")
+		os.system("gnome-terminal -e 'bash -c \"python -c 'print(\'OUTPUT FROM "+ file +"\'); python ./scorer/MAP_scripts/ev.py ./scorer/SemEval2016-Task3-CQA-QL-dev.xml.subtaskB.relevancy ./models/" + file +";exec bash\"'")
 
 
 os.system("gnome-terminal -e 'bash -c \"sl; exec bash\"'")
