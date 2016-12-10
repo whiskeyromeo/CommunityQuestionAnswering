ece10b7c085d113b5ea9b8770d0dfa8214bcbb88
Will Russell <whiskeyromeo06@gmail.com>
2016-12-10 14:32:52 -0500

    fixed rmodelRunner.py

--- FinalProject/modelRunner.py
+++ FinalProject/modelRunner.py
@@ -11,13 +11,27 @@ for file in glob.glob('*.pred'):
 	filenames.append(file)
 
 print('###################################')
-print('Getting Ready to run experiments')
-print('here we go...')
+print('''
+			This file will execute the MAP scorer on the .pred files
+			contained within the models folder. To proceed through 
+			the files, please press enter on this console window 
+			when you are ready to view the next set of results. 
 
+
+			Getting ready to run experiments....
+
+
+
+			Here we go....
+
+
+
+	''')
 os.chdir('..')
 for file in filenames:
 	print('###################################')
-	print(file)
+	print('Running MAP for :' + file)
+	print('Press enter when ready')
 	if 'subtaskA' in file:
 		os.system("gnome-terminal -e 'bash -c \"python ./scorer/MAP_scripts/ev.py ./scorer/SemEval2016-Task3-CQA-QL-dev-subtaskA.xml.subtaskA.relevancy ./models/" + file +";exec bash\"'")
 		raw_input()
@@ -25,3 +39,4 @@ for file in filenames:
 		os.system("gnome-terminal -e 'bash -c \"python ./scorer/MAP_scripts/ev.py ./scorer/SemEval2016-Task3-CQA-QL-dev.xml.subtaskB.relevancy ./models/" + file +";exec bash\"'")
 		raw_input()
 os.system("gnome-terminal -e 'bash -c \"sl; exec bash\"'")
+print('All prediction files executed. Have a nice day.')
