33cb9bf3c34bc93ecbe58561c1400ea6165f02c7
Will Russell <whiskeyromeo06@gmail.com>
2016-12-10 13:29:40 -0500

    fixed rmodelRunner.py

--- FinalProject/modelRunner.py
+++ FinalProject/modelRunner.py
@@ -1,3 +1,7 @@
+'''
+	Execution of this file will run all of the 
+'''
+
 import os
 import glob
 
@@ -6,10 +10,18 @@ filenames = []
 for file in glob.glob('*.pred'):
 	filenames.append(file)
 
+print('###################################')
+print('Getting Ready to run experiments')
+print('here we go...')
+print('###################################')
+
 os.chdir('..')
 for file in filenames:
-	os.system("gnome-terminal -e 'bash -c \"python .scorer/MAP_scripts/ev.py SemEval2016-Task3-CQA-QL-dev.xml.subtaskB.relevancy ./models/" + file +";exec bash\"'")
+	if 'subtaskA' in file:
+		os.system("gnome-terminal -e 'bash -c \"python ./scorer/MAP_scripts/ev.py SemEval2016-Task3-CQA-QL-dev-subtaskA.xml.subtaskA.relevancy ./models/" + file +";exec bash\"'")
+	else:
+		os.system("gnome-terminal -e 'bash -c \"python ./scorer/MAP_scripts/ev.py SemEval2016-Task3-CQA-QL-dev.xml.subtaskB.relevancy ./models/" + file +";exec bash\"'")
 
 
-os.system("gnome-terminal -e 'bash -c \"sl; exec bash\"'")
+os.system("gnome-terminal -e 'bash -c \"sudo apt-get install sl, sl; exec bash\"'")
 
